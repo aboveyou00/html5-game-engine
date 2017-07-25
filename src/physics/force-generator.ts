08554970c3b56df738f55333785eb0a27f452494
aboveyou00 <brandonyoyoslade@gmail.com>
2017-07-25 16:15:33 -0600

    fix(physics): make ForceGenerator an abstract class

--- src/physics/force-generator.ts
+++ src/physics/force-generator.ts
@@ -1,6 +1,6 @@
 import { CollisionMask } from './collision-mask';
 
-export class ForceGenerator {
-    updateCollider(collider: CollisionMask, delta: number);
+export abstract class ForceGenerator {
+    abstract updateCollider(collider: CollisionMask, delta: number);
     render(collider: CollisionMask, context: CanvasRenderingContext2D) { }
 }
