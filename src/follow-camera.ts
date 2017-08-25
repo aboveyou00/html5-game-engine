5d7fc3fa4645896a7ef8d1c9be80aeb385a2c2b3
Brandon <brandonyoyoslade@gmail.com>
2017-08-25 15:08:13 -0600

    feat(follow-camera): add horizontal clamp values to ensure the camera does not follow off of the map

--- src/follow-camera.ts
+++ src/follow-camera.ts
@@ -24,12 +24,18 @@ export class FollowCamera extends Camera {
         this._offset = [offsetx, offsety];
     }
 
+    clampLeft = -Infinity;
+    clampRight = Infinity;
+    
     //TODO: add beforeRender lifecycle hook; use that hook to avoid calling this method multiple times in one render cycle
     renderTransformed(adapter: GraphicsAdapter, act: () => void) {
         if (this.follow) {
             let target: [number, number] = [this._follow.x + this._offset[0], this._follow.y + this._offset[1]];
             this.center = target;
         }
+        let bounds = this.bounds;
+        if (bounds.right > this.clampRight) this.center = [this.center[0] - (bounds.right - this.clampRight), this.center[1]];
+        if (bounds.left < this.clampLeft) this.center = [this.center[0] + (this.clampLeft - bounds.left), this.center[1]];
         super.renderTransformed(adapter, act);
     }
 }
