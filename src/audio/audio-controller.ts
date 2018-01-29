c39464c0ca70d89577bb908aa55ec6b2aa527682
aboveyou00 <brandonyoyoslade@gmail.com>
2018-01-28 17:16:11 -0700

    feat(audio): ensure setVolume fails when val is not a number

--- src/audio/audio-controller.ts
+++ src/audio/audio-controller.ts
@@ -6,10 +6,13 @@ export class AudioController {
     
     private _volumes = new Map<string, number>();
     getVolume(channel: string) {
+        if (typeof channel !== 'string') throw new Error(`Invalid audio channel. Cannot get volume for channel ${channel}`);
         if (!this._volumes.has(channel)) return 1;
         return this._volumes.get(channel);
     }
     setVolume(channel: string, val: number) {
+        if (typeof val !== 'number') throw new Error(`Invalid volume. Cannot set volume to ${val}`);
+        if (typeof channel !== 'string') throw new Error(`Invalid audio channel. Cannot set volume for channel ${channel}`);
         val = clamp(val, 0, 1);
         let prev = this.getVolume(channel);
         if (val === prev) return;
